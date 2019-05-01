9d43bc4a176d487648e829962c58f6f8217f05db
Charlie Quin <charliequin@outlook.com>
2019-05-01 12:11:36 +0100

    Removed Unnecessary Comments

--- framed.js
+++ framed.js
@@ -8,14 +8,11 @@ function framed() {
     let answer = readlineSync.question('Enter words separated by space: ');
     let sortArray = answer.split(' ');
 
-    // Top box border.
     let number = longestItem(sortArray);
 
-    // Sets top padding number to equal length of array item lines.
     let maxCharPadding = number + 4;
     let maxChar = number + 1;
 
-    // Multiplies the asterisk's by the highest value.
     let boxPad = '*';
     console.log('\n' + boxPad.repeat(maxCharPadding));
 
@@ -23,7 +20,6 @@ function framed() {
         console.log('* ' + (sortArray[i].padEnd(maxChar, ' ') + '*'));
     };
 
-    // Bottom Box border.
     console.log(boxPad.repeat(maxCharPadding));
 };
 
@@ -49,20 +45,14 @@ function max(wordArray) {
 function longestItem(wordArray) {
 
     let lengthArray = [];
-
-    // Gets the length of each array item.
     for (i = 0; i < wordArray.length; i++) {
         lengthArray.push(wordArray[i].length);
     };
 
-    // Determines the highest value in the length array.
     let maxChar = max(lengthArray);
-
     return maxChar;
 };
 
 
 
-
-// Executes the main function.
 framed();
