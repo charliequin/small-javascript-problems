628d84f93027e86b680590fc2b325550c6ec83b5
Charlie Quin <charliequin@outlook.com>
2019-05-01 12:12:18 +0100

    Machine Guesses Number
    Like guess.js however the machine guesses a number I am thinking.

---
+++ user-guess.js
@@ -0,0 +1,38 @@
+const readlineSync = require('readline-sync');
+
+
+let highestNum = 100;
+let lowestNum = 0;
+
+let tooHigh = 'Too High';
+let tooLow = 'Too Low';
+let correct = 'Correct';
+
+
+for (let i = 0; i <= 10; i++) {
+
+    let range = highestNum + lowestNum;
+    let middle = Math.round(range / 2);
+
+    let guess = readlineSync.question('Is your number ' + middle + '? ');
+
+    if (guess === tooLow) {
+        lowestNum = middle;
+    }
+
+    if (guess  === tooHigh) {
+        highestNum = middle;
+    } 
+
+    if (guess  === correct) {
+        console.log('\nI win!');
+        console.log('I took '+ i + ' attempts.')
+        break;
+    } else {
+        console.log("\nYou're cheating, aren't you?");
+    }
+}
+
+
+
+
